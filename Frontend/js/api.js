5dcd6cda900382c0a4433f3ab23cf3d831e7dab2
mamirjonovmppwork <mamirjonovmppwork@gmail.com>
2026-06-22 21:39:49 +0500

    Frontend API manzili Renderga sozlandi

--- Frontend/js/api.js
+++ Frontend/js/api.js
@@ -1,5 +1,5 @@
 // Backend server porti va manzili
-const BASE_URL = 'http://localhost:5000/api'; 
+const BASE_URL = 'https://trading-jurnal.onrender.com'; 
 
 const api = {
     // --- GET SO'ROVLARI ---
