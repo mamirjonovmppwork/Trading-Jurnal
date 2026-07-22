eb5791dacb8d784ee95735eecbde68545d67a554
mamirjonovmppwork <mamirjonovmppwork@gmail.com>
2026-07-22 09:36:15 +0500

    versel cors xatos FX

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ app.get("/health", (req, res) => {
 app.use(cors({
     // ⚠️ BU YERGA O'ZINGIZNING NETLIFY HAVOLANGIZNI YOZING! (oxiridagi / belgisiz)
     origin: [
-        'https://trading-jurnalv2.netlify.app', 
+        'https://trading-jurnal-two.vercel.app', // 🟢 QO'SHILDI — Vercel'dagi frontend
         'http://localhost:5500', // Mahalliy tekshirishlar uchun ham joy qoldiramiz
         'http://127.0.0.1:5500'
     ],
